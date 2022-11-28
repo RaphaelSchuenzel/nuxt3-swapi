2e743a0e80df572e28f7b6ca0440acda53e83510
Raphael Schünzel <inbox+git@raphaelschuenzel.com>
2022-11-28 02:35:10 +0100

    basic character list implementation

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,30 @@
 <template lang="pug">
-div
-    p Character List
+#characters
+    .character(
+        v-for="character in data.results"
+    )
+        NuxtLink(
+            :to="getCharacterUrl(character.url)"
+        ) {{ character.name }}
 </template>
+
+<script lang="ts">
+const baseURL = `https://swapi.dev/api/`
+
+export default defineNuxtComponent({
+    async asyncData () {
+        return {
+            data: await $fetch(`people`, { baseURL })
+        }
+    },
+    methods: {
+        getCharacterUrl: (url: String) => {
+            const match = url.match(`/people/([0-9]+)/`)
+
+            return (match && match[1]) ? `/characters/${match[1]}` : false
+        }
+    }
+})
+</script>
 
 <style lang="sass" scoped></style>
