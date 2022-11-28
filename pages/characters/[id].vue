0c731e0653c2fc9a17d240815a609af3e109c247
Raphael Schünzel <inbox+git@raphaelschuenzel.com>
2022-11-28 21:32:47 +0100

    add character page functionality

--- pages/characters/[id].vue
+++ pages/characters/[id].vue
@@ -1,6 +1,33 @@
 <template lang="pug">
 div
-    p Character {{ $route.params.id }}
+    h1 {{ character.name }}
+
+    div
+        label Species
+        p {{ character.species }}
+
+    div
+        label Movies
+        p {{ character.films }}
+
+    div
+        label Spaceships
+        p {{ character.starships }}
 </template>
+
+<script lang="ts">
+const baseURL = `https://swapi.dev/api/`
+
+export default defineNuxtComponent({
+    async asyncData () {
+        const route = useRoute()
+        const character = await $fetch(`people/${route.params.id}/`, { baseURL })
+
+        return {
+            character
+        }
+    }
+})
+</script>
 
 <style lang="sass" scoped></style>
